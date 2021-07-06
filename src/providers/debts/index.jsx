3060324f7eee00dd6d4e36ef5465399c4278e6a0
Allef Moraes <allefadolpho@hotmail.com>
2021-07-06 18:46:28 -0300

    feat: Debts Context Provider
    Consumed the endpoints needed to create debts. Made and persisted the
    elements in the useDebts hook.

---
+++ src/providers/debts/index.jsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../../services";
+import {useUser} from "../users";
+
+export const DebtContext = createContext([])
+
+export const DebtProvider = ({children}) => {
+    const {token} = useUser()
+    const [debtCreateSuccess, setDebtCreateSuccess] = useState(Boolean)
+    const [debtEditSuccess, setDebtEditSuccess] = useState(Boolean)
+    const [debtDeleteSuccess, setDebtDeleteSuccess] = useState(Boolean)
+    const [debts,setDebts] = useState([])
+
+    useEffect(() => {
+        if(token !== "") {
+            api.get("debt", {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then((res) => {
+                setDebts(res.data)
+            })
+        }
+    }, [debtCreateSuccess,debtEditSuccess,debtDeleteSuccess])
+
+    const createDebt = (data) => {
+        api.post("debt", data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((res) => {
+            if(res.status === 201) {
+                setDebtCreateSuccess(true)
+            }
+        }).catch((_) => setDebtCreateSuccess(false))
+    }
+
+    const editDebt = (data,id) => {
+        api.patch(`debt/${id}`, data, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((_) => {
+            setDebtEditSuccess(true)
+        }).catch((_) => setDebtEditSuccess(false))
+    }
+
+    const deleteDebt = (data) => {
+        api.delete(`debt/${data}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((_) => {
+            setDebtDeleteSuccess(true)
+        }).catch((_) => setDebtDeleteSuccess(false))
+    }
+
+    return (
+        <DebtContext.Provider
+            value={{createDebt, editDebt, deleteDebt,
+                    debts, debtCreateSuccess, debtEditSuccess, debtDeleteSuccess}}
+        >
+            {children}
+        </DebtContext.Provider>
+    )
+
+}
+
+export const useDebts = () => useContext(DebtContext)
