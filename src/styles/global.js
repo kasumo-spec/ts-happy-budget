d2bc66ffdefb98707a081e91c30e03d6d1fce499
Leomar Romanzini <leomarromanzini@gmail.com>
2021-07-07 23:50:19 -0300

    add: images login and register page

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,7 +4,7 @@ export const GlobalStyles = createGlobalStyle`
     :root {
         //primary's colors:
         --white: #fff9ff;
-        --white-green: #EDFCF9;
+        --white-green: #DCF9F6;
         --black: #18251D;
         --violet: #341E48;
         --blue-green: #053E61;
@@ -51,7 +51,8 @@ export const GlobalStyles = createGlobalStyle`
 
     a {
         text-decoration: none;
-        color: blue;
+        color: #0049A3;
+        font-weight: 500;
     }
 
     button {
@@ -64,8 +65,9 @@ export const GlobalStyles = createGlobalStyle`
     }
 
     body {
-        padding-top: 90px;
-        background-color: var(--white)
+        margin-top: 95px;
+        background-color: var(--white-green);
+        
     }
 
  
