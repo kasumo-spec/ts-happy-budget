9831cd5124f9695980cf83af658fa9a1bf0f9ff0
Diogo <diogosantosferreira.01@gmail.com>
2021-07-13 18:12:01 -0300

    style: styling Tips

--- src/components/TipsItems/styles.js
+++ src/components/TipsItems/styles.js
@@ -1,12 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    padding: 20px;
+    padding: 23px;
+    display: flex;
+    justify-content: space-around;
+    
+    width: 100%;
+
+    .text{
+        width: 70%; 
+    }
+    .image{
+        width: 30%;
+        padding: 0 10px;
+        margin-top: 53px;
+    }
 
     p{
         padding: 6px;
         text-align: justify;
-        margin: 9px 5px;
+        margin: 7px 5px;
     }
     h2{
         font-size: 1.3em;
@@ -14,4 +27,28 @@ export const Container = styled.div`
         margin: 3px;
         text-align: center;
     }
+    img{
+        width: 100%;
+        height: 350px;
+    }
+    .box{
+        display: flex;
+        justify-content: space-around;
+        margin: 28px 0;
+    }
+    .box img{
+        margin-top: -28px;
+    }
+    .imageSmall{
+        height: 100%;
+        margin-top: -28px;
+    }
+    .imgDecimo{
+        height: 187px;
+    }
+    @media screen and (max-width: 540px) {
+    .image{
+        display: none;
+    }
+  }
 `;
